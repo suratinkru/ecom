f4ac8b16e99f61babbb356f2f4b775ae873ff687
suratinkru <toey0908toey@gmail.com>
2022-12-17 17:04:39 +0700

    home

--- index.php
+++ index.php
@@ -110,35 +110,142 @@ include_once "./controllers/index.php";
         font-size: 14px;
 
     }
+
+    .lineunder {
+
+        animation-duration: 3s;
+        animation-name: slidein;
+        animation-iteration-count: infinite;
+        animation-direction: alternate;
+        color: #333333;
+
+
+
+    }
+
+    @keyframes slidein {
+        from {
+            margin-left: 10%;
+            width: 100%;
+        }
+
+        to {
+            margin-left: 0%;
+            width: 100%;
+        }
+    }
+
+    .imgAnimate {
+
+        animation-duration: 3s;
+        animation-name: imgani;
+        animation-iteration-count: infinite;
+        animation-direction: alternate;
+        /* transform: translate(284px, 442px) translate(-50%, -50%) translate3d(0px, 1.6427px, 0px) translate(0px, 2px) rotateX(-0.6975deg);  */
+
+    }
+
+    @keyframes imgani {
+        from {
+
+            transform: translate(2px, 42px) translate(-0%, -0%) translate3d(20px, 5.27px, 0px) translate(0px, 1px) rotateX(-50.6975deg);
+            /* margin-top: 5%; */
+            /* width: 100%; */
+        }
+
+        to {
+
+            transform: translate(54px, 62px) translate(-30%, -30%) translate3d(0px, 1.6427px, 0px) translate(0px, 2px) rotateX(-0.6975deg);
+            /* margin-top: 0%; */
+            /* width: 100%; */
+        }
+    }
+
+    .title {
+        color: #faea55;
+
+text-shadow: 4px 3px 0px #7A7A7A;
+    }
+
+    .ctname {
+        text-align: center;
+    color: #313231;
+    text-shadow: 1px 1px 0px #ffffff;
+    font-weight: bold;
+    font-size: 18px;
+    }
+
+
+
+
+
+
+
+    .imgAnimateCategory {
+
+animation-duration: 3s;
+animation-name: imganic;
+animation-iteration-count: infinite;
+animation-direction: alternate;
+/* transform: translate(284px, 442px) translate(-50%, -50%) translate3d(0px, 1.6427px, 0px) translate(0px, 2px) rotateX(-0.6975deg);  */
+
+}
+
+@keyframes imganic {
+from {
+
+   
+    margin-top: 2%;
+    /* width: 100%; */
+}
+
+to {
+
+
+    margin-top: 0%;
+    /* width: 100%; */
+}
+}
 </style>
+
+
+
 <section id="hero" class="d-flex align-items-center">
     <div class="container">
         <div class="row">
             <div class="col-lg-6 d-lg-flex flex-lg-column justify-content-center align-items-stretch pt-5 pt-lg-0 order-2 order-lg-1 aos-init aos-animate" data-aos="fade-up">
                 <div>
-                    <h1>Cat stores</h1>
-                    <h2 class="mb-5">อาหารแมว ขนมสัตว์เลี้ยง ของใช้สัตว์เลี้ยง ผลิตภัณฑ์อาบน้ำ/บำรุงขน เกรดพรีเมี่ยม</h2>
+                    <h1 class="title">Cat stores</h1>
+                    <div style="height: 90px;">
+                    <h2 id="demo" class="mb-5  "></h2>
+                    </div>
+                   
                     <div class="d-flex flex-row mb-3 mt-5">
-                        <?php 
+                        <?php
 
                         $ct = $selectCategory->fetchAll(PDO::FETCH_ASSOC);
                         if (!empty($ct)) {
 
                             foreach ($ct as $row => $item) { ?>
-                        <div class="p-2"><img src="./admin/uploads/<?php echo $item['image']  ?>" class="rounded-circle" alt="..." width="100" height="100">
-                            <p style="text-align: center;"><?php echo $item['name']  ?></p>
-                        </div>
+                                <div class="p-2 text-center "><img src="./admin/uploads/<?php echo $item['image']  ?>" class="rounded-circle" alt="..." width="100" height="100">
+                                    <p class="ctname"><?php echo $item['name']  ?></p>
+                                </div>
 
-                        <?php }}   ?>
-               
+                        <?php }
+                        }   ?>
+
                     </div>
                     <a href="product.php" class="btn btn-danger"><i class="fa-brands fa-shopify me-2"></i>ซื้อเลย</a>
                 </div>
             </div>
-            <div class="col-lg-6 d-lg-flex flex-lg-column align-items-stretch order-1 order-lg-2 hero-img aos-init aos-animate" data-aos="fade-up"> <img src="./assets/images/ch.jpg" class="img-fluid" alt=""></div>
+            <div class="col-lg-6 d-lg-flex flex-lg-column align-items-center order-1 order-lg-2 hero-img aos-init aos-animate imgAnimate " data-aos="fade-up"> <img src="./assets/images/SW.png" width="400" alt=""></div>
         </div>
     </div>
 </section>
+
+
+
+
 
 
 <div class="container mt-5">
@@ -146,45 +253,64 @@ include_once "./controllers/index.php";
 
     <div class="row row-cols-1 row-cols-md-6 g-1 mb-5  text-center p-1">
 
-    <?php 
-    $Pd = $selectProduct->fetchAll(PDO::FETCH_ASSOC);
-                        if (!empty($Pd)) {
+        <?php
+        $Pd = $selectProduct->fetchAll(PDO::FETCH_ASSOC);
+        if (!empty($Pd)) {
 
-                            foreach ($Pd as $row => $item) { ?>
+            foreach ($Pd as $row => $item) { ?>
 
-        <div class="col-12">
-            <a href="#">
-                <div class="card h-100">
-                    <img src="./assets/images/ch.jpg" class="card-img-top" alt="..." style="height: 150px; object-fit: cover;">
-                    <div class="card-body">
-                        <h5 class="card-title text-start text-dart"><?php echo $item['name']  ?></h5>
+                <div class="col-12">
+                    <a href="product_detail.php?id=<?php echo $item['id'] ?>">
+                        <div class="card h-100">
+                            <img src="./assets/images/ch.jpg" class="card-img-top" alt="..." style="height: 150px; object-fit: cover;">
+                            <div class="card-body">
+                                <h5 class="card-title text-start text-dart"><?php echo $item['name']  ?></h5>
 
 
-                        <div class="price d-flex flex-row align-items-center"> <span class="act-price">฿ <?php echo $item["price"] - $item["discount"] ;?></span>
-                            <div class="ml-2"> <small class="dis-price">  <?php echo $item["price"];?> </small> <span>- <?php echo $item["discount"] * 100 / $item["price"];?>%  </span> </div>
-                        </div>
-                        <div class=" d-flex justify-content-between align-items-center">
-                            <div class="small-ratings">
-                                <i class="fa fa-star rating-color" style="font-size: 10px;"></i>
-                                <i class="fa fa-star rating-color" style="font-size: 10px;"></i>
-                                <i class="fa fa-star rating-color" style="font-size: 10px;"></i>
-                                <i class="fa fa-star rating-color" style="font-size: 10px;"></i>
-                                <i class="fa fa-star rating-color" style="font-size: 10px;"></i>
-                                <span style="font-size: 10px;">(10)</span>
+                                <div class="price d-flex flex-row align-items-center"> <span class="act-price">฿ <?php echo $item["price"] - $item["discount"]; ?></span>
+                                    <div class="ml-2"> <small class="dis-price"> <?php echo $item["price"]; ?> </small> <span>- <?php echo $item["discount"] * 100 / $item["price"]; ?>% </span> </div>
+                                </div>
+                                <div class=" d-flex justify-content-between align-items-center">
+                                    <div class="small-ratings">
+                                        <i class="fa fa-star rating-color" style="font-size: 10px;"></i>
+                                        <i class="fa fa-star rating-color" style="font-size: 10px;"></i>
+                                        <i class="fa fa-star rating-color" style="font-size: 10px;"></i>
+                                        <i class="fa fa-star rating-color" style="font-size: 10px;"></i>
+                                        <i class="fa fa-star rating-color" style="font-size: 10px;"></i>
+                                        <span style="font-size: 10px;"><?php echo $item["qty"]; ?></span>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
-
-                    </div>
+                    </a>
                 </div>
-            </a>
-        </div>
 
-        <?php }}   ?>
+        <?php }
+        }   ?>
 
 
         <!-- end สินค้า -->
     </div>
 </div>
+
+<script>
+var i = 0;
+var txt = 'อาหารแมว ขนมสัตว์เลี้ยง ของใช้สัตว์เลี้ยง ผลิตภัณฑ์อาบน้ำ/บำรุงขน เกรดพรีเมี่ยม';
+var speed = 100;
+typeWriter();
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }else{
+   document.getElementById("demo").innerHTML = "";
+   i =0;
+  typeWriter();
+  }
+}
+</script>
 
 <?php
 
--- layouts/footer.php
+++ layouts/footer.php
@@ -1,6 +1,55 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+<footer class="bg-primary py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 text-white">
+        <h4>About</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ornare, elit at dictum dictum, nisi nisi dictum nisi, eu ultricies nisi lacus a enim. Duis ullamcorper vehicula fermentum. Donec rutrum dapibus lacus, eu interdum dolor tincidunt vel.</p>
+      </div>
+      <div class="col-md-4 text-white">
+        <h4>Links</h4>
+        <ul class="list-unstyled">
+          <li><a href="#" class="text-white">Home</a></li>
+          <li><a href="#" class="text-white">About</a></li>
+          <li><a href="#" class="text-white">Contact</a></li>
+        </ul>
+      </div>
+      <div class="col-md-4 text-white">
+        <h4>Contact</h4>
+        <p>
+          123 Main Street<br>
+          Anytown, USA 12345<br>
+          <a href="mailto:info@example.com" class="text-white">info@example.com</a>
+        </p>
+      </div>
+    </div>
+    <div class="row py-3 mt-5 border-top border-white">
+      <div class="col-md-6 text-md-left text-white">
+        Copyright &copy; 2021 Your Website
+      </div>
+      <div class="col-md-6 text-md-right">
+        <a href="#" class="text-white"><i class="fab fa-twitter fa-lg"></i></a>
+        <a href="#" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+        <a href="#" class="text-white"><i class="fab fa-instagram fa-lg"></i></a>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
 <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
 </body>
